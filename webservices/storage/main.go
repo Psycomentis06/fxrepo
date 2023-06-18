306d8d29def1a045df7bca8a729ef965b0d5b66b
psycomentis06 <alibenamor30@gmail.com>
2023-06-18 17:53:31 +0100

    Image routes + Middlewares

--- webservices/storage/main.go
+++ webservices/storage/main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"log"
+	"storage/src/config"
+	"storage/src/controllers"
+	"storage/src/middlewares"
+	"storage/src/services"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/api/v1/api_access/new", func(c *gin.Context) {
+	r := gin.New()
+	cnf := config.GetEnvConfig()
+	mc := services.NewS3Client(&cnf)
+	ctx := context.Background()
+	r.Use(gin.Logger(), gin.Recovery(), middlewares.InjectionMiddleware(&ctx, &cnf, mc))
+
+	_, cbErr := services.CreateBucket(ctx, mc, config.Configuration.ImageBucketName)
+	if cbErr != nil {
+		log.Fatal(cbErr)
+	} else {
+		log.Print("Bucket Image created")
+	}
+
+	r.GET("/", func(c *gin.Context) {
+		/*
+			envCnf := services.GetEnvConfigContext(c)
+			mc := services.NewS3Client(envCnf)
+			ctx := context.Background()
+		*/
 		c.JSON(200, gin.H{
-			"message": "Generate an api access",
+			"Message": "msg",
 		})
+
 	})
+	apiV1Grp := r.Group("/api/v1")
+	{
+		imageGrp := apiV1Grp.Group("/image")
+		{
+			imageGrp.GET(":fileId", controllers.GetImage)
+			imageGrp.POST("", controllers.UploadImg)
+			imageGrp.DELETE(":fileId", controllers.RemoveImg)
+		}
+	}
+
 	err := r.Run()
 	if err != nil {
 		log.Panicf(err.Error())
