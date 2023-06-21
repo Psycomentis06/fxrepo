442f94065978d690083f0c759dcbda1785a371b1
psycomentis06 <alibenamor30@gmail.com>
2023-06-21 17:15:59 +0100

    Spring Init project

---
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/FxRepoMainApplication.java
@@ -0,0 +1,13 @@
+package com.github.psycomentis06.fxrepomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxRepoMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxRepoMainApplication.class, args);
+	}
+
+}
---
+++ webservices/fx-repo-main/src/test/java/com/github/psycomentis06/fxrepomain/FxRepoMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.psycomentis06.fxrepomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxRepoMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
