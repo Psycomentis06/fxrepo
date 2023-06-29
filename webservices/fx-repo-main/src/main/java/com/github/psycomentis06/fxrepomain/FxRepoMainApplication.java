b0f1afde6ea89a5c90ef1a0117f5f5261809caf7
psycomentis06 <alibenamor30@gmail.com>
2023-06-29 14:31:53 +0100

    Logging StorageService::init

--- webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/FxRepoMainApplication.java
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/FxRepoMainApplication.java
@@ -2,12 +2,14 @@ package com.github.psycomentis06.fxrepomain;
 
 import com.github.psycomentis06.fxrepomain.properties.StorageProperties;
 import com.github.psycomentis06.fxrepomain.service.StorageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+@Slf4j(topic = "Main")
 @SpringBootApplication
 @EnableConfigurationProperties(
 		StorageProperties.class
@@ -21,7 +23,12 @@ public class FxRepoMainApplication {
 	@Bean
 	public CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
-			storageService.init();
+			var s = storageService.init();
+			switch (s) {
+				case UPLOAD_DIR_NOT_CREATED -> log.warn("Upload directory not created");
+				case UPLOAD_DIR_CREATED -> log.info("Upload directory created");
+				default -> log.info("Upload dir: No action taken");
+			}
 		};
 	}
 
