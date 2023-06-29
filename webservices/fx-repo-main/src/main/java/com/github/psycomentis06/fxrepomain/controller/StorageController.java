ba85db2a3abbff9a6c924d3554eafb8c8c25d6ea
psycomentis06 <alibenamor30@gmail.com>
2023-06-29 14:32:44 +0100

    Upload response conditions

--- webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/controller/StorageController.java
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/controller/StorageController.java
@@ -1,9 +1,12 @@
 package com.github.psycomentis06.fxrepomain.controller;
 
+import com.github.psycomentis06.fxrepomain.model.ResponseObjModel;
 import com.github.psycomentis06.fxrepomain.service.StorageService;
 import com.github.psycomentis06.fxrepomain.service.StorageServiceStatus;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,24 +22,30 @@ public class StorageController {
         this.storageService = storageService;
     }
     @PostMapping("upload")
-    public String upload(
+    public ResponseEntity<ResponseObjModel> upload(
             @RequestParam("file") MultipartFile file
     ){
+        var resObj = new ResponseObjModel();
         if (file == null) {
-            return "File can't be null";
+            resObj.setData(null)
+                    .setMessage("\"file\" parameter is null")
+                    .setCode(HttpStatus.BAD_REQUEST.value())
+                    .setStatus(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(resObj, HttpStatus.BAD_REQUEST);
         }
         var f = storageService.store(file);
-        switch (f) {
-            case FILE_NOT_STORED -> {
-                return "File not stored";
-            }
-            case FILE_STORED -> {
-                return "File stored";
-            }
-            default -> {
-                return "Default";
-            }
+        if (f == null) {
+            resObj.setData(null)
+                    .setMessage("File not stored")
+                    .setCode(HttpStatus.FORBIDDEN.value())
+                    .setStatus(HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>(resObj, HttpStatus.FORBIDDEN);
         }
+        resObj.setData(f)
+                .setMessage("File stored")
+                .setStatus(HttpStatus.OK)
+                .setCode(HttpStatus.OK.value());
+        return new ResponseEntity<>(resObj, HttpStatus.OK);
     }
 
 
