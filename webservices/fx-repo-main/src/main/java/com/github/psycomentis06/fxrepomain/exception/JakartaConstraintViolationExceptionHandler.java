2b313d72d870c1f5a512c2bdb72f684fa6b35348
psycomentis06 <alibenamor30@gmail.com>
2023-08-05 14:51:23 +0100

    ConstraintViolation handler

--- webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/exception/JakartaConstraintViolationExceptionHandler.java
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/exception/JakartaConstraintViolationExceptionHandler.java
@@ -5,12 +5,16 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.Timestamp;
 import java.util.HashMap;
 
 @Slf4j
+@RestControllerAdvice
 public class JakartaConstraintViolationExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ExceptionViolationModel> violation(ConstraintViolationException e) {
         log.error(e.getMessage(), e);
         HashMap<String, String> map = new HashMap<>();
@@ -23,7 +27,7 @@ public class JakartaConstraintViolationExceptionHandler {
                 .setTimestamp(new Timestamp(System.currentTimeMillis()))
                 .setCode(HttpStatus.BAD_REQUEST.value())
                 .setStatus(HttpStatus.BAD_REQUEST)
-                .setMessage(e.getMessage());
+                .setMessage("Failed to continue the operation due to constraints violation");
         return new ResponseEntity<>(m, m.getStatus());
     }
 }
