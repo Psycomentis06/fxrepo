821a0b9db8967039e2309862fc9029593cad75ef
psycomentis06 <alibenamor30@gmail.com>
2023-08-06 19:01:02 +0100

    Redis config

---
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/config/AppConfig.java
@@ -0,0 +1,33 @@
+package com.github.psycomentis06.fxrepomain.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String hostname;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        var config = new RedisStandaloneConfiguration(hostname, port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        return template;
+    }
+}
