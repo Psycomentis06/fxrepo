cb1c2e9bb66531141d76ef80c2ffd07528700c9a
psycomentis06 <alibenamor30@gmail.com>
2023-08-15 13:36:24 +0100

    Add orphan flag

--- webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/entity/File.java
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/entity/File.java
@@ -3,7 +3,6 @@ package com.github.psycomentis06.fxrepomain.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.minidev.json.annotate.JsonIgnore;
 
 import java.util.Set;
 
@@ -16,8 +15,13 @@ public class File {
     @Id
     private String id;
 
-    @Enumerated
+    @Enumerated(EnumType.STRING)
     private FileServicePlacement placement = FileServicePlacement.MAIN_SERVICE;
+
+    // Flag indicating to the Garbage Collector (Not the VM GC it's our little program that removes
+    // unused saved files) that this file is not necessarily linked to a post but
+    // should not be removed/cleaned
+    private boolean orphan = false;
 
     @OneToMany(cascade = CascadeType.ALL)
     private Set<FileVariant> variants;
