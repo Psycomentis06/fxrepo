bb457d4d4106d2e3a2cf25c320806b18fd06b876
psycomentis06 <alibenamor30@gmail.com>
2023-09-29 00:44:08 +0100

    fix mkdir condition

--- webservices/preprocessing/setup.sh
+++ webservices/preprocessing/setup.sh
@@ -2,8 +2,12 @@
 
 MODELS_PATH="./models"
 NSFW_MODEL_PATH="$MODELS_PATH/nsfw.299x299.h5"
-
 # Download NSFW prediction model
+
+if [ ! -d $MODELS_PATH ]; then
+    mkdir -p $MODELS_PATH
+fi
+
 if [ ! -f $NSFW_MODEL_PATH ]; then
     echo "NSFW model not found. Downloading..."
     wget -P $MODELS_PATH https://s3.amazonaws.com/ir_public/ai/nsfw_models/nsfw.299x299.h5
