a87f3e6313f5fe922ad963b5c072825e6760f0d0
psycomentis06 <alibenamor30@gmail.com>
2023-09-28 23:34:19 +0100

    Models dir

---
+++ webservices/preprocessing/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MODELS_PATH="./models"
+NSFW_MODEL_PATH="$MODELS_PATH/nsfw.299x299.h5"
+
+# Download NSFW prediction model
+if [ ! -f $NSFW_MODEL_PATH ]; then
+    echo "NSFW model not found. Downloading..."
+    wget -P $MODELS_PATH https://s3.amazonaws.com/ir_public/ai/nsfw_models/nsfw.299x299.h5
+    echo "NSFW model downloaded."
+  else
+    echo "NSFW model found. Skipping download."
+fi
