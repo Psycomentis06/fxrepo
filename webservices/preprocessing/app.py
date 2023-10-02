23ef83b5045d796d892608c4e1a72befe2ba4451
psycomentis06 <alibenamor30@gmail.com>
2023-10-02 19:31:17 +0100

    Prepare ImageTopicsHandler

--- webservices/preprocessing/app.py
+++ webservices/preprocessing/app.py
@@ -1,9 +1,17 @@
 import sys
-from confluent_kafka import Consumer, KafkaException, KafkaError
+from confluent_kafka import KafkaException
 from containers import root_container
-from services.kafka import KafkaTopics
+from services.kafka import KafkaTopics, decode
+from services.types.kafka_image import ImagePostData
+from services.types.kafka_common import KafkaData
 
 logger = root_container.logger_service()
+
+
+def image_topic_handler(msg: str):
+    image_data = decode(msg, KafkaData[ImagePostData])
+    logger.info("Received image data from event: {}".format(image_data['eventId']))
+    logger.info("Received image data: {}".format(image_data['payload']))
 
 
 def consume_image_kafka_topic():
@@ -16,15 +24,21 @@ def consume_image_kafka_topic():
             msg = consumer.poll(timeout=1.0)
             if msg is None: continue
             if msg.error():
-                if msg.error().code() == KafkaError._PARTITION_EOF:
+                if msg.error().code() == -191:  # KafkaError._PARTITION_EOF
                     sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
                                      (msg.topic(), msg.partition(), msg.offset()))
                 elif msg.error():
                     raise KafkaException(msg.error())
             else:
-                logger.info("Received message: {}".format(msg.value().decode('utf-8')))
+                logger.info("Received message")
                 logger.info(msg.topic())
                 # message_object = kafka.encode(msg)
+                match msg.topic():
+                    case KafkaTopics.IMAGE:
+                        logger.info("Image topic event event")
+                        image_topic_handler(msg.value().decode('utf-8'))
+                    case _:
+                        logger.warn("Unhandled topic: {}".format(msg.topic()))
     finally:
         consumer.close()
 
