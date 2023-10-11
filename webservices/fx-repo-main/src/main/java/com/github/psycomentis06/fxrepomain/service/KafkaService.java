3948731619d4470b88423fae22d7c3e5399d2292
psycomentis06 <alibenamor30@gmail.com>
2023-10-11 23:18:00 +0100

    Topics

--- webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/service/KafkaService.java
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/service/KafkaService.java
@@ -4,6 +4,9 @@ import com.github.psycomentis06.fxrepomain.entity.ImagePost;
 
 public interface KafkaService {
     final static String IMAGE_TOPIC = "fx_repo_topics_image";
+    final static String VIDEO_TOPIC = "fx_repo_topics_video";
+    final static String AUDIO_TOPIC = "fx_repo_topics_audio";
+    final static String VECTOR_TOPIC = "fx_repo_topics_vector";
 
     void publishNewImagePostEvent(ImagePost p);
 
