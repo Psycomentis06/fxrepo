b5514d65c2155300e0aaa2b21dfa0ee76fb363cd
psycomentis06 <alibenamor30@gmail.com>
2023-10-11 23:19:10 +0100

    Handle kafkaEvent#1

--- webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/FxRepoMainApplication.java
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/FxRepoMainApplication.java
@@ -1,10 +1,21 @@
 package com.github.psycomentis06.fxrepomain;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.psycomentis06.fxrepomain.entity.ImagePost;
+import com.github.psycomentis06.fxrepomain.model.kafka.Action;
+import com.github.psycomentis06.fxrepomain.model.kafka.KafkaEventModel;
+import com.github.psycomentis06.fxrepomain.model.kafka.Status;
+import com.github.psycomentis06.fxrepomain.model.kafka.Target;
 import com.github.psycomentis06.fxrepomain.properties.StorageProperties;
+import com.github.psycomentis06.fxrepomain.repository.KafkaEventRepository;
+import com.github.psycomentis06.fxrepomain.service.ImagePostService;
 import com.github.psycomentis06.fxrepomain.service.KafkaService;
 import com.github.psycomentis06.fxrepomain.service.StorageService;
 import com.github.psycomentis06.fxrepomain.service.TypesenseService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,15 +30,66 @@ import org.springframework.kafka.annotation.KafkaListener;
 )
 public class FxRepoMainApplication {
 
+    private KafkaEventRepository kafkaEventRepository;
+    private ImagePostService imagePostService;
+
+    public FxRepoMainApplication(KafkaEventRepository kafkaEventRepository, ImagePostService imagePostService) {
+        this.kafkaEventRepository = kafkaEventRepository;
+        this.imagePostService = imagePostService;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(FxRepoMainApplication.class, args);
     }
 
-    @KafkaListener(topics = KafkaService.IMAGE_TOPIC)
-    public void imageTopicListener(String in) {
-        System.out.println("***************Image topic sub*****************");
-        System.out.println(in);
-        System.out.println("***********************************************");
+    @KafkaListener(topics = {KafkaService.IMAGE_TOPIC})
+    public void imageTopicListener(ConsumerRecord<String, String> in) {
+        var objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        switch (in.topic()) {
+            case KafkaService.IMAGE_TOPIC -> {
+                try {
+                    var kafkaEvent = objectMapper.readValue(in.value(), KafkaEventModel.class);
+                    log.info("--Image topic-- Received event: {}", kafkaEvent.eventId());
+                    var savedKafkaEvent = kafkaEventRepository.findById(kafkaEvent.eventId());
+                    if (savedKafkaEvent.isPresent()) {
+                        log.info("--Image topic-- Event already exists: {}", kafkaEvent.eventId());
+                    } else {
+                        if (kafkaEvent.targets().contains(Target.MAIN)
+                                && kafkaEvent.status().equals(Status.DONE)
+                                && kafkaEvent.action().equals(Action.PROCESSING)) {
+//                            var imagePostType = objectMapper.getTypeFactory().constructParametricType(KafkaEventModel.class, ImagePost.class);
+//                            var kafkaImagePost = objectMapper.readValue(in.value(), imagePostType);
+                            var kafkaImagePost = objectMapper.convertValue(kafkaEvent.payload(), ImagePost.class);
+                            System.out.println(kafkaImagePost.getThumbnail());
+                            /*imagePostService.preprocessingUpdatePostImage(kafkaImagePost);
+                            KafkaEvent k = new KafkaEvent();
+                            k.setId(kafkaEvent.eventId());
+                            k.setTime(LocalDateTime.parse(kafkaEvent.eventTime()));
+                            k.setStatus(Status.DONE);
+                            k.setMessage(in.value());
+                            kafkaEventRepository.save(k);*/
+                        } else {
+                            log.info("Not the target");
+                        }
+                    }
+                } catch (JsonProcessingException e) {
+                    log.error("Error parsing json", e);
+                }
+            }
+            case KafkaService.VIDEO_TOPIC -> {
+                log.info("Video topic");
+            }
+            case KafkaService.AUDIO_TOPIC -> {
+                log.info("Audio topic");
+            }
+            case KafkaService.VECTOR_TOPIC -> {
+                log.info("Vector topic");
+            }
+            default -> {
+                log.info("Unknown topic");
+            }
+        }
     }
 
     @Bean
