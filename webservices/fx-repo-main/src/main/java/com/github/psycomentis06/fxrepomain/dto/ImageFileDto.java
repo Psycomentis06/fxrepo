802f0978fe4e4c8d2aa21d8c1ffba812693d08f8
psycomentis06 <alibenamor30@gmail.com>
2023-10-13 23:12:23 +0100

    accentColor & colorPalette convert HashMap and Set<HashMap> to json string.

--- webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/dto/ImageFileDto.java
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/dto/ImageFileDto.java
@@ -3,13 +3,48 @@ package com.github.psycomentis06.fxrepomain.dto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.HashMap;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class ImageFileDto extends FileDto {
-    private String accentColor;
-    private String colorPalette;
+    private HashMap<String, String> accentColor;
+    private Set<HashMap<String, String>> colorPalette;
     private boolean landscape;
     private Set<ImagePostDto> posts;
+
+    public String getAccentColor() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        accentColor.forEach((k, v) -> sb.append("\"").append(k).append("\":\"").append(v).append("\","));
+        sb.append("}");
+        return sb.toString();
+    }
+
+    public void setAccentColor(HashMap<String, String> accentColor) {
+        this.accentColor = accentColor;
+    }
+
+    public String getColorPalette() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        AtomicInteger index = new AtomicInteger(0);
+        colorPalette.forEach(item -> {
+            sb.append("{");
+            item.forEach((k, v) -> sb.append("\"").append(k).append("\":\"").append(v).append("\","));
+            sb.append("}");
+            if (index.get() < colorPalette.size()) {
+                sb.append(",");
+            }
+            index.getAndIncrement();
+        });
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public void setColorPalette(Set<HashMap<String, String>> colorPalette) {
+        this.colorPalette = colorPalette;
+    }
 }
