59d1e6fc23c2c03136051814b716cff9fab22fd8
psycomentis06 <alibenamor30@gmail.com>
2023-10-18 17:10:43 +0100

    Light image processing for initial validation. (only interface no impl)

---
+++ webservices/fx-repo-main/src/main/java/com/github/psycomentis06/fxrepomain/service/ProcessImageService.java
@@ -0,0 +1,9 @@
+package com.github.psycomentis06.fxrepomain.service;
+
+public interface ProcessImageService {
+    boolean isTypeValid();
+
+    String getImageType();
+
+    String getImageQuality();
+}
