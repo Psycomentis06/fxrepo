4e5eea0b110873d47b82a5268cd55ffbeb914072
psycomentis06 <psycomentis06@localhost.localdomain>
2023-11-25 21:26:03 +0100

    Fix Category Autocomplete

--- clients/web-client/src/app/home/image/post/components/create-image-form/create-image-form.component.ts
+++ clients/web-client/src/app/home/image/post/components/create-image-form/create-image-form.component.ts
@@ -1,8 +1,16 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {CategoryService} from "../../../../../services/category.service";
-import {Category} from "../../../../../models/category";
-import {map, Observable, tap} from "rxjs";
+import {debounceTime, distinctUntilChanged, map, Observable} from "rxjs";
+
+type PostImageForm = {
+  title: FormControl,
+  description: FormControl,
+  category: FormControl,
+  visibility: FormControl,
+  tags: FormControl,
+  nsfw: FormControl
+}
 
 @Component({
   selector: 'app-create-image-form',
@@ -18,28 +26,50 @@ export class CreateImageFormComponent implements OnInit {
 
   dataForm: FormGroup
   categories: Observable<string[]>
-  category: string = ""
+  filteredCategories: Observable<string[]>
 
   constructor(private categoryService: CategoryService) {
     this.dataForm = new FormGroup({})
     this.categories = new Observable<string[]>()
+    this.filteredCategories = new Observable<string[]>()
   }
 
   ngOnInit() {
-    this.categories = this.categoryService.getCategories("image", "", 0, 5)
-      .pipe(
-        map((res) => res.data.content.map(c => c.name.length === 0 ? "default" : c.name)
-        )
-      )
+    this._filter("")
 
-    this.dataForm = new FormGroup<any>({
+    this.dataForm = new FormGroup<PostImageForm>({
       title: new FormControl(''),
       description: new FormControl(''),
-      category: new FormControl(this.category),
+      category: new FormControl(),
       visibility: new FormControl('true'),
       tags: new FormControl(''),
       nsfw: new FormControl(false),
     })
+
+    this.dataForm.get("category")?.valueChanges
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+      )
+      .subscribe(val => {
+        this._filter(val)
+      })
+  }
+
+  private _filter(value: string) {
+    const filterValue = value.toLowerCase()
+    this.categories = this.categoryService.getCategories("image", filterValue, 0, 5)
+      .pipe(
+        map((res) => res.data.content.map(c => c.name.length === 0 ? "default" : c.name))
+      )
+  }
+
+  getCategories(query: string) {
+    return this.categoryService.getCategories("image", query, 0, 5)
+  }
+
+  inputChangeEvent(event: Event) {
+    console.log(event)
   }
 
   submit(e: Event) {
