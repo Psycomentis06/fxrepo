ee19cf35480831674176f972a4d2e24cab021530
psycomentis06 <psycomentis06@localhost.localdomain>
2023-11-29 12:26:04 +0100

    Login & Register Components

--- clients/web-client/src/app/core/core.module.ts
+++ clients/web-client/src/app/core/core.module.ts
@@ -9,10 +9,12 @@ import {DesktopComponent} from './components/header/components/desktop/desktop.c
 import { NotificationsComponent } from './components/notifications/notifications.component';
 import { ToastsComponent } from './components/toasts/toasts.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent, MobileComponent, DesktopComponent, NotificationsComponent, ToastsComponent, ModalComponent],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, MobileComponent, DesktopComponent, NotificationsComponent, ToastsComponent, ModalComponent, LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     RouterModule,
